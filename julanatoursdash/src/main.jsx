199d67444a844d6605bad7e9cdd8c7d341bd256a
InduruN <indurunimesha@gmail.com>
2025-06-28 02:45:15 +0530

    all are added

---
+++ julanatoursdash/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
+
+export default main;
