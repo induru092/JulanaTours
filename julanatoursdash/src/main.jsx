289eee1cab1ee38aa87b4d7a2330cb047ceabda5
chamirudilan20 <155279548+chamirudilan20@users.noreply.github.com>
2025-07-08 10:16:44 +0530

    Main.jsx was updated

--- julanatoursdash/src/main.jsx
+++ julanatoursdash/src/main.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import  React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter } from 'react-router-dom';
+import { StoreContextProvider } from './context/StoreContext.jsx';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+ ReactDOM.createRoot(document.getElementById('root')).render(
+   <BrowserRouter>
+      <StoreContextProvider>
+         <App/>
+      </StoreContextProvider>
+   </BrowserRouter>
+  
 )
