6ccb4f4ea062a022831e8bdba9eddf85b4a79cdd
KavindyaL <kavindyalakshani123@gmail.com>
2025-07-10 11:09:36 +0530

    Added cart button

--- julanatoursdash/src/pages/VehicleDetails/VehicleDetails.jsx
+++ julanatoursdash/src/pages/VehicleDetails/VehicleDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { fetchVehicleDetails } from '../../service/vehicleService';
 
 export const VehicleDetails = () => {
     const {id} = useParams();
@@ -7,24 +8,29 @@ export const VehicleDetails = () => {
     const [data, setData] = useState({}) ;
     
     useEffect(() => {
-        fetchVehicleDetails(id);
+        const loadVehicleDetails = async () => {
+           try {
+                const vehicleData = await fetchVehicleDetails(id);
+                setData(vehicleData);
+           } catch (error) {
+                toast.error('Error displaying the vehicle details.');           
+           }
+        }
     }, [id]);
-    
+    loadVehicleDetails()
   return (
     <section className="py-5">
             <div className="container px-4 px-lg-5 my-5">
                 <div className="row gx-4 gx-lg-5 align-items-center">
-                    <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
+                    <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src={data.imageUrl} alt="..." /></div>
                     <div className="col-md-6">
-                        <div className="small mb-1">SKU: BST-498</div>
-                        <h1 className="display-5 fw-bolder">Shop item template</h1>
+                        <div className="small mb-1">Category: <span className='badge text-bg-warning'>{data.category}</span></div>
+                        <h1 className="display-5 fw-bolder">{data.name}</h1>
                         <div className="fs-5 mb-5">
-                            <span className="text-decoration-line-through">$45.00</span>
-                            <span>$40.00</span>
+                            <span>&#8377:{data.price}</span>
                         </div>
-                        <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
+                        <p className="lead">{data.description}</p>
                         <div className="d-flex">
-                            <input className="form-control text-center me-3" id="inputQuantity" type="num" style={{"maxWidth": "3rem"}} />
                             <button className="btn btn-outline-dark flex-shrink-0" type="button">
                                 <i className="bi-cart-fill me-1"></i>
                                 Add to cart
