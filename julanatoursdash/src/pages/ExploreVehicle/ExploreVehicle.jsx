4fd2139d0ec7afd90963b87411d565aa5d8ba918
KavindyaL <kavindyalakshani123@gmail.com>
2025-07-10 19:27:41 +0530

    Add a category to ExploreVehicle.jsx

--- julanatoursdash/src/pages/ExploreVehicle/ExploreVehicle.jsx
+++ julanatoursdash/src/pages/ExploreVehicle/ExploreVehicle.jsx
@@ -3,7 +3,29 @@ import VehicleDisplay from '../../components/VehicleDisplay/VehicleDisply';
 
 export const ExploreVehicle = () => {
   return (
-    <VehicleDisplay />
+    <>
+     <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <form>
+            <div className="input-group mb-3">
+              <select className="form-select mt-2" style={{'maxWidth': '150px'}}>
+                <option value="all">All Vehicles</option>
+                <option value="cars">Cars</option>
+                <option value="bikes">Bikes</option>
+                <option value="trucks">Trucks</option>
+              </select>
+              <input type='text' className='form-control mt-2' placeholder='Search your favourite dish...'/>
+              <button className='btn btn-primary mt-2' type='submit'>
+                <i className='bi bi-search'></i>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <VehicleDisplay /> 
+    </>
   )
 }
 
