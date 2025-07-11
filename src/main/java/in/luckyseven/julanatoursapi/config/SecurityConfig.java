2b7ff7f52d79eef3822493d9f8e6f95ee0be0c0c
kavisha616 <kavishachamodya@gmail.com>
2025-07-11 18:41:22 +0530

    created the java class JwtAuthenticationFilter. Made changes to JwtUtil and SecurityConfig classes.

--- src/main/java/in/luckyseven/julanatoursapi/config/SecurityConfig.java
+++ src/main/java/in/luckyseven/julanatoursapi/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package in.luckyseven.julanatoursapi.config;
 
 
+import in.luckyseven.julanatoursapi.filters.JwtAuthenticationFilter;
 import in.luckyseven.julanatoursapi.service.AppUserDetailsService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -28,6 +30,7 @@ import java.util.List;
 public class SecurityConfig {
 
     private final AppUserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -35,8 +38,9 @@ public class SecurityConfig {
             http
                     .cors(Customizer.withDefaults())
                     .csrf(AbstractHttpConfigurer::disable)
-                    .authorizeHttpRequests(auth -> auth.requestMatchers(...patterns:"/api/register", "/api/login", "/api/vehicles/**").permitAll().anyRequest().authenticated())
+                    .authorizeHttpRequests(auth -> auth.requestMatchers("/api/register", "/api/login", "/api/vehicles/**").permitAll().anyRequest().authenticated())
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
             return http.build();
 
         }
