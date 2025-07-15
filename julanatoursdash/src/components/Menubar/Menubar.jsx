b9c65df969229278fdd7412f8ef5c1df6a66a407
KavindyaL <kavindyalakshani123@gmail.com>
2025-07-15 11:15:53 +0530

    Changed  Menubar.css

--- julanatoursdash/src/components/Menubar/Menubar.jsx
+++ julanatoursdash/src/components/Menubar/Menubar.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Menubar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import {assets} from '../../assets/assests.js';
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext.jsx';
 
 export const Menubar = () => {
+  const [active, setActive] = useSte("home");
+  const {quantities} = useContext[StoreContext];
+  const uniqueItemsInCart = Object.values(quantities).filter(qty => qty > 0).length;
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container">
@@ -16,20 +20,28 @@ export const Menubar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link"  to="/">Home</Link>
+          <Link className={active === 'home' ? "nav-link fw-bold active": "nav-link"}  to="/" onClick={() => setActive('home')}>
+            Home
+          </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/explore">Explore</Link>
+          <Link className={active === 'explore' ? "nav-link fw-bold active": "nav-link"} to="/explore" onClick={() => setActive('explore')}>
+            Explore
+          </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/contact">Contact Us</Link>
+          <Link className={active === 'contact-us' ? "nav-link fw-bold active": "nav-link"} to="/contact"  onClick={() => setActive('contact-us')}>
+            Contact Us
+          </Link>
         </li>
       </ul>
       <div className="d-flex align-items-center gap-4">
         <Link to={'/cart'}>
             <div className="position-relative">
             <img src={assets.cart} alt="" height={32} width={32} className='position-relative'/>
-            <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning'>5</span>
+            <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning'>
+              {uniqueItemsInCart}
+            </span>
         </div>
         </Link>
         <button className='btn btn-outline-primary'>Login</button>
