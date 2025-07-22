df091bcd3437fdd4f44882bc30c9b847ec0cfa87
KavindyaL <kavindyalakshani123@gmail.com>
2025-07-22 17:47:47 +0530

    Created vehicleService.js

---
+++ julanatoursdash/src/Service/vehicleService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:8080/api/vehicles';
+
+export const fetchVehiclesList = async () => {
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.log('Error fetching vehicle list:'.error);
+        throw error;
+    }
+}
+
+export const fetchVehicleDetails = async (id) => {
+    try {
+        const response = await axios.get[API_URL+"/" + id];
+        return response.data;
+    }  catch (error) {
+        console.log('Error fetching the vehicle details:',error);
+        throw error;
+    }
+    }
--- julanatoursdash/src/main.jsx
+++ julanatoursdash/src/main.jsx
@@ -1,19 +1,20 @@
-import  React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter } from 'react-router-dom';
 import { StoreContextProvider } from './context/StoreContext.jsx';
 
 
- ReactDOM.createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
-      <StoreContextProvider>
-         <App/>
-      </StoreContextProvider>
-   </BrowserRouter>
-  
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <StoreContextProvider>
+            <App />
+        </StoreContextProvider>
+    </BrowserRouter>
 );
