1f52b6fe5bcac39d9f03212cd292b26794fed8aa
KavindyaL <kavindyalakshani123@gmail.com>
2025-07-23 14:47:50 +0530

    Added post

--- julanatoursdash/src/context/authService.js
+++ julanatoursdash/src/context/authService.js
@@ -5,12 +5,18 @@ const API_URL = "http://localhost:8080/api/";
 export const registerUser = async (data) => {
     try {
         const response = await axios.post(
-        API_URL+"register",
-        data
+            API_URL+"register",
+            data
         );
         return response
     }catch(error){
         throw error;
+    }    
+}
+export const login = async (data) => {
+    try {
+        axios.post('')
+    }catch (error){
+
     }
-    
-    }
+}
